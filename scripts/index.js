3a7d34a93b6612fa6ed6ebc9fb008e8da6e2fa3d
Anastasiya Ivanchenko <Krylovaanasteisha98@yandex.ru>
2024-06-27 17:00:39 +0300

    5ПР

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
 // @todo: Темплейт карточки
-
-// @todo: DOM узлы
+const containerCard = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+initialCards.forEach(function (element)  {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardButtonDelete = cardElement.querySelector('.card__delete-button');
+
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__image').alt = element.name;
+  cardElement.querySelector('.card__title').textContent = element.name;
+
+  cardButtonDelete.addEventListener('click', deleteCard);
+
+  // @todo: Вывести карточки на страницу
+  containerCard.append(cardElement);
+});
 
 // @todo: Функция удаления карточки
+  function deleteCard() {
+    const cardButtonDelete = document.querySelector('.card__delete-button');
 
-// @todo: Вывести карточки на страницу
+    const containerDelete = cardButtonDelete.closest('.card');
+    containerDelete.remove();
+  }
